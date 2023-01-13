b130dd7ddc8a97062e0e040840f988f24df0a78d
noaholatoye <noaholatoye@gmail.com>
2023-01-13 15:29:46 +0100

    Debugging keys

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -77,6 +77,7 @@ class Login extends React.Component {
 
     let myHeaders = new Headers();
     myHeaders.append(SK_KEY, SK_VALUE);
+    console.log(SK_KEY, SK_VALUE);
 
 
     // get assessment from db
