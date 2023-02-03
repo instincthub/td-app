fac94ffd68f532e80206f3bcf2705bee9d8bc826
noaholatoye <noaholatoye@gmail.com>
2023-02-03 03:25:34 +0100

    Production logs

--- src/pages/QuizResults.jsx
+++ src/pages/QuizResults.jsx
@@ -12,6 +12,8 @@ import { PageLoading } from "../components/forms/PageLoading";
 
 function QuizResults() {
     const [data, setData] = useState([])
+    const [status, setStatus] = useState([])
+    const [error, setError] = useState([])
 	loginRequired(getCookie('access'))
 
 	useEffect(() => {
@@ -24,7 +26,7 @@ function QuizResults() {
 
 		const requestOptions =  reqOptions("GET", null, true)
 
-		fetchAPI(setData, `${HOST_URL()}/api/v1/assessment/answer/results/${u_id}/${assessment_id}/${counts}/`, requestOptions, true)
+		fetchAPI(setData, `${HOST_URL()}/api/v1/assessment/answer/results/${u_id}/${assessment_id}/${counts}/`, requestOptions, true, setStatus, setError)
 
 		  
 	// eslint-disable-next-line
@@ -69,7 +71,7 @@ function QuizResults() {
 			</div>
 		);
 	}
-	else if (data.status === 'false'){
+	else if (error.status === "false"){
 		return(
 			<div>
 				<Navbar />
