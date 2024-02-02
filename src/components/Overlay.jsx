ce199702275ff31e4afcd368629d0784fa263fa8
noaholatoye <noaholatoye@gmail.com>
2024-02-02 16:01:58 +0100

    Added how to register video to overlay

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -2,26 +2,38 @@ import React from "react";
 import { removeVideo } from "./static/assets/js/nav";
 
 const Overlay = () => {
-  return (
-    <>
-      <div className="play_video container" id="video_holder"
-      onClick={()=>{
-        document.querySelector(".play_video").style.display = "none";
-        }}
-        
-        itemScope itemType="https://ft.intsincthub.com/enrol">
-        <img
-          src={require("./static/assets/svgs/close_icon_white.svg").default}
-          alt=""
-          className="close_svg"
-          onClick={removeVideo}
-        />
-        <div className="video_frame container">
-          <iframe itemProp="Video" title="Tech Diversity in Niger Overview" src="https://player.vimeo.com/video/668725125?h=5533a272ad" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div
+				className="play_video container"
+				id="video_holder"
+				onClick={() => {
+					document.querySelector(".play_video").style.display = "none";
+				}}
+				itemScope
+				itemType="https://ft.intsincthub.com/enrol"
+			>
+				<img
+					src={require("./static/assets/svgs/close_icon_white.svg").default}
+					alt=""
+					className="close_svg"
+					onClick={removeVideo}
+				/>
+				<div className="video_frame container">
+					<iframe
+						itemProp="Video"
+						title="Tech Diversity in Niger Overview"
+						src="https://www.youtube.com/embed/QkLLdoJ0cAA?si=dcykPkAZ8Ne4lAiA"
+						width="640"
+						height="564"
+						frameborder="0"
+						allow="autoplay; fullscreen"
+						allowfullscreen
+					></iframe>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Overlay;
